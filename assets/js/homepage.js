6333a123f660302e7497041c1b03e90f72cccaa9
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-06-09 17:49:50 -0400

    adding javascript

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,21 +1,45 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSeachTerm = document.querySelector("#repo-search-term");
+
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     var userFormEl = document.querySelector("#user-form");
     var nameInputEl = document.querySelector("#username");
-  
+
+    
     // make a request to the url
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
-        console.log(data);
+        displayRepos(data, user);
       });
     });
   };
 
   var formSubmitHandler = function(event) {
     event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
     console.log(event);
-  };
+    };
+
+    var displayRepos = function(repos, searchTerm) {
+        console.log(repos);
+        console.log(searchTerm);
+        // clear old content
+        repoContainerEl.textContent = "";
+        repoSearchTerm.textContent = searchTerm;
+    };
 
 
 
